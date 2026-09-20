edeaa28183ecad68eb5ee6a60bf4c7f37da91967
Mohanad Ahmed <mohanad071071@gmail.com>
2026-09-20 06:34:34 +0300

    add tagging print message

--- index.java
+++ index.java
@@ -3,6 +3,7 @@ public class index{
         System.out.println("Hello");
         int x = 10;
         System.out.println(x);
-	System.out.println("change and merge"); //i try to merge branch 
+	System.out.println("change and merge"); //i try to merge branch
+	System.out.println("tagging somthing in the project"); 
     }
 }
